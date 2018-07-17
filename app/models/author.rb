09c8e3f63bf5883ca3d5a7dac994adb1fb4bc52d
Nick Wohnhas <nicholas.wohnhas@powerhrg.com>
2018-07-17 19:10:08 -0400

    form_tag validations

--- app/models/author.rb
+++ app/models/author.rb
@@ -1,4 +1,8 @@
 # frozen_string_literal: true
 
 class Author < ActiveRecord::Base
+  validates :name, presence: true
+  validates :phone_number, length: { is: 10 }
+  validates :name, uniqueness: true
+  validates :email, uniqueness: { message: "need unique email." }
 end
