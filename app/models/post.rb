9f80d845b06a08e7f52fadca9375ffca7f2c046b
Kevin Skilton <kevin.skilton@powerhrg.com>
2019-05-18 18:57:08 -0400

    done

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,4 +1,7 @@
 # frozen_string_literal: true
 
 class Post < ActiveRecord::Base
+  validates :title, length: { minimum: 2 }
+  validates :content, length: { minimum: 100 }
+  validates :category, inclusion: { in: %w[Fiction Non-Fiction] }
 end
